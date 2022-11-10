7c5c19d82994ee27f1376fdbd5712cf79116d461
bananarama92 <105885570+bananarama92@users.noreply.github.com>
2022-11-10 16:05:51 +0100

    MAINT: Change the .yaml style to be more BC-friendly

--- lint.py
+++ lint.py
@@ -1,34 +1,9 @@
 """A script for linting .yaml files."""
 
-from __future__ import annotations
-
 import argparse
-from collections.abc import Iterable
-from typing import TYPE_CHECKING, Any
-
 import yaml
 
-if TYPE_CHECKING:
-    from yaml.emitter import _WriteStream
-
-
-class Representer(yaml.representer.SafeRepresenter):
-    def represent_list(self, data: Iterable[object]) -> yaml.SequenceNode:
-        return self.represent_sequence('tag:yaml.org,2002:seq', data, flow_style=True)
-
-
-Representer.add_representer(list, Representer.represent_list)
-
-
-class Dumper(Representer, yaml.SafeDumper):
-    def __init__(self, stream: _WriteStream, **kwargs: Any) -> None:
-        super().__init__(stream, **kwargs)
-        Representer.__init__(
-            self,
-            default_style=kwargs.get("default_style", None),
-            default_flow_style=kwargs.get("default_flow_style", False),
-            sort_keys=kwargs.get("sort_keys", True),
-        )
+NO_COMMA = frozenset({"admin_list", "ban_list"})
 
 
 def main(filename: str) -> None:
@@ -36,8 +11,13 @@ def main(filename: str) -> None:
         dct = yaml.load(f1.read(), Loader=yaml.SafeLoader)
 
     with open(filename, "w", encoding="utf8") as f2:
-        yaml.dump(dct, f2, Dumper=Dumper, sort_keys=False)
-
+        for k, v in dct.items():
+            if isinstance(v, list):
+                v.sort()
+                sep = "," if k in NO_COMMA else ", "
+                f2.write(f"{k}: [{sep.join(repr(i) for i in v)}]\n")
+            else:
+                f2.write(f"{k}: {v!r}\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage="python ./lint.py room_settings.yaml", description=__doc__)
